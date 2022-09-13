27efef2b78f9b6a8915115198e825f33ef66ca91
Amanda Minatto <amanda_minatto@hotmail.com>
2022-09-12 21:49:15 -0300

    Alura-Studies

---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,27 @@
+import { ITarefa } from '../../types/tarefa';
+import style from './lista.module.scss';
+import Item from "./Item";
+
+interface Props {
+    tarefas: ITarefa[],
+    selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
+
+function Lista({tarefas, selecionaTarefa}: Props) {
+    return (
+        <aside className={style.listaTarefas}>
+            <h2> Estudos do dia</h2>
+            <ul>
+                {tarefas.map(item => (
+                    <Item
+                        selecionaTarefa={selecionaTarefa}
+                        key = {item.id}
+                        {...item}
+                    />                  
+                ))}
+            </ul>
+        </aside>
+    )
+}
+
+export default Lista;
